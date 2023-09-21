aad0bf0274ca92b852bab656259e6ad8ce1a6e75
david <dbpintogonza@gmail.com>
2023-09-21 16:10:18 -0500

    05

--- login/LoginAtenea.cs
+++ login/LoginAtenea.cs
@@ -16,5 +16,54 @@ namespace appFP_CSharp.login
         {
             InitializeComponent();
         }
+
+        private void LoginMaestro_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("solo letras", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+             if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("solo números", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); 
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            txtPassword.PasswordChar = '*';
+        }
+
+        private void Ocultar_Click(object sender, EventArgs e)
+        {
+            Mostar.BringToFront();
+            txtPassword.PasswordChar = '*';
+
+        }
+
+        private void Mostar_Click(object sender, EventArgs e)
+        {
+            Ocultar.BringToFront();
+            txtPassword.PasswordChar = '\0';
+
+        }
     }
 }
